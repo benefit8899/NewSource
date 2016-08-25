4bdb8c2c1380f99b0ca9d9e6503852ea40a9565a
benefit8899 <vanloi.infotech@gmail.com>
2016-08-25 17:37:10 +0700

    s

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.Write("Holi");
+            Console.Write("Holii");
             Console.Read();
 
 
