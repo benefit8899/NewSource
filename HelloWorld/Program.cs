0c7bfc96918c3300b2157af053bdc8001cb7940b
benefit8899 <vanloi.infotech@gmail.com>
2016-08-25 17:25:46 +0700

    ss

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
+            Console.Write("Holoe");
             Console.Read();
         }
     }
